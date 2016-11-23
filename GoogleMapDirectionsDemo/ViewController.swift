e52c3ce8b6c725b1bbb798b7794ebcf7be154238
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-11-23 20:41:30 +0700

    - Initial Project.

---
+++ GoogleMapDirectionsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMapDirectionsDemo
+//
+//  Created by Kittitat Rodphotong on 11/23/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
