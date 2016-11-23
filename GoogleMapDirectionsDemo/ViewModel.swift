aea8ab0bb9b9e8c5258c64e36520a4da5a92859b
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-11-23 21:12:31 +0700

    - Create `ViewModel`.

---
+++ GoogleMapDirectionsDemo/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  GoogleMapDirectionsDemo
+//
+//  Created by Kittitat Rodphotong on 11/23/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
